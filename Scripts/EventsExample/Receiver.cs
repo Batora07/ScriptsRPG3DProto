c849434ae992e99d976198087930a5bfdf22d7b1
Marc Portet <marcsuitgundamseed@hotmail.fr>
2018-06-24 12:13:47 +0200

    Add event / delegates examples
    Add events examples

---
+++ Scripts/EventsExample/Receiver.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Receiver : MonoBehaviour {
+
+// pipeline is Awake / OnEnable / Start
+	void Start(){
+
+	}
+
+	void OnEnable(){
+		// Subscribe to event
+		Sender.playerDiedInfo += PlayerDiedListener;
+	}
+
+	void OnDisable(){
+		// Unsub to prevent memory leak
+		Sender.playerDiedInfo -= PlayerDiedListener;
+	}
+
+	float PlayerDiedListener(Vector3 player, Vector3 target){
+		print("Function is called. Distance is " + Vector3.Distance(player, target));
+		return Vector3.Distance(player, target);
+	}
+}
---
+++ Scripts/EventsExample/Sender.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sender : MonoBehaviour {
+
+	public delegate float PlayerDied(Vector3 player, Vector3 target);
+	public static event PlayerDied playerDiedInfo;
+
+	private bool isAlive;
+
+	void Start(){
+		Invoke("ExecuteEvent",3f);
+	}
+
+	void ExecuteEvent(){
+		// useful to prevent nullref in the case no object is subscribed to this event
+		if(playerDiedInfo != null){
+			playerDiedInfo(new Vector3(1f,1f,1f), new Vector3(2f,2f,2f));
+		}
+	}
+	
+}
